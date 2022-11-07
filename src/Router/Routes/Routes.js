7ed970d1be992a4683c1ba09b1c8afd688a12eda
afzal75 <afzalbhuiyan16@gmail.com>
2022-11-08 02:11:30 +0600

    dynamic title & favicon added

---
+++ src/Router/Routes/Routes.js
@@ -0,0 +1,25 @@
+import Main from "../../Layout/Main";
+import Home from "../../Pages/Home/Home/Home";
+import ErrorPage from "../../Pages/Shared/ErrorPage/ErrorPage";
+
+const { createBrowserRouter } = require("react-router-dom");
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Main></Main>,
+        errorElement: <ErrorPage></ErrorPage>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            },
+            {
+                path: '/home',
+                element: <Home></Home>
+            },
+        ]
+    }
+])
+
+export default router;
