d43c7089ac6d850bcd707a30dd7c0561052911c1
afzal75 <afzalbhuiyan16@gmail.com>
2022-11-08 20:46:45 +0600

    react-photo-view implement done

--- src/Pages/CardDetails/CardDetails.jsx
+++ src/Pages/CardDetails/CardDetails.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import 'react-photo-view/dist/react-photo-view.css';
+import { PhotoProvider, PhotoView } from 'react-photo-view';
 import { useLoaderData } from 'react-router-dom';
 
 const CardDetails = () => {
@@ -6,7 +8,11 @@ const CardDetails = () => {
     return (
         <div className='w-1/2 m-auto'>
             <div className="card card-compact w-90 bg-base-100 shadow-xl">
-                <figure><img src={img} alt="Dental" /></figure>
+                <PhotoProvider>
+                    <PhotoView src={img}>
+                    <figure><img src={img} alt="" /></figure>
+                    </PhotoView>
+                </PhotoProvider>
                 <div className="card-body">
                     <h2 className="card-title">{name}</h2>
                     <p className='text-2xl text-orange-600 font-semibold'>Price: ${price}</p>
